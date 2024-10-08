2fed45698065f8bd93a1de12f732fe8cc9899746
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-10-08 12:58:02 -0300

    feat: create healthcheck endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Declare an instance of the config struct.
+	var cfc config
+
+	// Read the value of the port and env command-line flags into the config struct. We default to using
+	// the port number 4000 and the environment "development" if no corresponding flags are provided.
+	flag.IntVar(&cfc.port, "port", 4000, "API server port")
+	flag.StringVar(&cfc.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	// Initialize a new structured logger which writes log entries to the standard out stream.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// Declare an instance of the application struct, containing the config struct and the logger.
+	app := &application{
+		config: cfc,
+		logger: logger,
+	}
+
+	//  Declare a new servemux and add a /v1/healthcheck route which dispatches requests to the healthcheckHandler
+	//  method (which we will create in a moment).
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// Declare an HTTP server which listens on the port provided in the config struct, uses the servemux we created
+	// above as the handler, has some sensible timeout settings and writes any log messages to the structured logger
+	// at Error level.
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfc.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	// Start the HTTP server.
+	logger.Info("starting server", "addr", srv.Addr, "env", cfc.env)
+
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
 }
