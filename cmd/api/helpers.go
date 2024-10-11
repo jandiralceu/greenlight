f33eba32874413cc84760507fa4dace6fcb1926d
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-10-11 13:20:53 -0300

    feat: customize bad request error

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,9 +3,12 @@ package main
 import (
 	"encoding/json"
 	"errors"
-	"github.com/julienschmidt/httprouter"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) readIDParam(r *http.Request) (int64, error) {
@@ -34,6 +37,36 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data any, h
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(result)
+
+	return nil
+}
+
+func (app *application) readJSON(_ http.ResponseWriter, r *http.Request, destination any) error {
+	if err := json.NewDecoder(r.Body).Decode(destination); err != nil {
+		var (
+			syntaxError           *json.SyntaxError
+			unmarshalTypeError    *json.UnmarshalTypeError
+			invalidUnmarshalError *json.InvalidUnmarshalError
+		)
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+		default:
+			return err
+		}
+	}
 
 	return nil
 }
