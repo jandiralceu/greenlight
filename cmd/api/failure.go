40259665a8408eb9ea8276fb8fd7a70e06d9c85d
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-10-21 03:32:41 -0300

    feat: add timeout

--- cmd/api/failure.go
+++ cmd/api/failure.go
@@ -23,7 +23,7 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.logError(r, err)
 
-	message := "the requested resource could not be found"
+	message := "the server encountered a problem and could not process your request"
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
 }
 
