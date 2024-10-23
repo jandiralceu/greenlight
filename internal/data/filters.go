dc414b94c9ab0c58894af1274e330467152daa88
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-10-22 21:57:46 -0300

    feat: add ascending and descending

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -25,14 +25,22 @@ func (f Filters) sortColumn() string {
 	panic("unsafe sort parameter: " + f.Sort)
 }
 
-// Return the sort direction ("ASC" or "DESC") depending on the prefix character of the 
+// Return the sort direction ("ASC" or "DESC") depending on the prefix character of the
 // Sort field.
 func (f Filters) sortDirection() string {
 	if strings.HasPrefix(f.Sort, "-") {
 		return "DESC"
 	}
-	
+
 	return "ASC"
+}
+
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
