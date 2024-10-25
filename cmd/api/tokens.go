3ae132b5d41eacccc71b8609825aea2d5936ed2f
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2024-10-25 17:06:42 -0300

    feat: create activation token

---
+++ cmd/api/tokens.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"errors"
+	"github.com/jandiralceu/greenlight/internal/data"
+	"github.com/jandiralceu/greenlight/internal/validator"
+	"net/http"
+	"time"
+)
+
+func (app *application) createActivationTokenHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Email string `json:"email"`
+	}
+
+	if err := app.readJSON(w, r, &input); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	if data.ValidateEmail(v, input.Email); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	user, err := app.models.Users.GetByEmail(input.Email)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			v.AddError("email", "no matching record found")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+
+		return
+	}
+
+	if user.Activated {
+		v.AddError("email", "user has already been activated")
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	app.background(func() {
+		values := map[string]interface{}{
+			"activationToken": token.PlainText,
+		}
+
+		if err := app.mailer.Send(user.Email, "token_activation.tmpl", values); err != nil {
+			app.logger.Error(err.Error())
+		}
+	})
+
+	message := map[string]interface{}{
+		"message": "an email will be sent to you containing activation instructions",
+	}
+
+	if err := app.writeJSON(w, http.StatusCreated, message, nil); err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
